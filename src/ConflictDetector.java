6fee79e984462c3fc2fad0ddea0a0d12704889ca
markuzzz <mark@markusbouwman.nl>
2015-06-04 19:04:03 +0200

    finished implementing operations ConflictDetector
    The operations in ConflictDetector are implemented. They wont work yet
    because the quadtree is not finished

--- src/ConflictDetector.java
+++ src/ConflictDetector.java
@@ -1,4 +1,5 @@
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -25,26 +26,50 @@ public class ConflictDetector implements ConflictDetectorInterface {
 
     @Override
     public int getActDegree(Label l) {
-        if (model.equals("1slider")) return getActConflictLabelsSlider(l).size();
+        if (model.equals("1slider")) return getActConflictLabelsSlider((SliderLabel) l).size();
         else return getActConflictLabelsDiscrete(l).size();
     }
     
     @Override
     public Set<Label> getActConflictLabels(Label l) {
-        if (model.equals("1slider")) return getActConflictLabelsSlider(l);
+        if (model.equals("1slider")) return getActConflictLabelsSlider((SliderLabel) l);
         else return getActConflictLabelsDiscrete(l);
     }
     
-    private Set<Label> getActConflictLabelsSlider(Label l) {
-        Set<Label> posConflics = getPosConflictLabels(l);
-        //TO DO: compute the actually conflicting labels
-        return posConflics;
+    private Set<Label> getActConflictLabelsSlider(SliderLabel l) {
+        Set<Label> result = new HashSet<Label>();
+        Set<Label> posLabels = new HashSet<Label>(l.getPoint().getPossibleLabels());
+        Set<Label> posConflicts = new HashSet<Label>(); //pos conflicts of PosLabels
+        
+        //get all the labels the Slider might intersect with
+        for (Label label : posLabels) {
+            posConflicts.addAll(getPosConflictLabels(l));
+        }
+        
+        //get all the associated Slider Labels
+        Set<SliderLabel> posConflictSlider = new HashSet<SliderLabel>(); //pos conflicts SliderLabels
+        for(Label label : posConflicts) {
+            posConflictSlider.add(label.getPoint().getActiveLabelSlider());
+        }
+        
+        //check if they indeed overlap and add to result
+        for(Label label: posConflictSlider) {
+            if(label.overlaps(l)) {
+                result.add(label);
+            }
+        }
+        return result;
     }
     
     private Set<Label> getActConflictLabelsDiscrete(Label l) {
-        Set<Label> posConflics = getPosConflictLabels(l);
-        //TO DO: determine which labels are active
-        return posConflics;
+        Set<Label> result = new HashSet<Label>();
+        Set<Label> posConflicts = getPosConflictLabels(l); //labels conflicting with given label
+        for(Label posLabel : posConflicts) { //for all the conflicting labels...
+            if(posLabel.equals(posLabel.getPoint().getActiveLabelPos())) { //...check if label is active
+                result.add(posLabel); //add to result
+            }
+        }
+        return result;
     }
 
     @Override
