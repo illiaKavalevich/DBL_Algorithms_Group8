81216b469385345b0c8281d68979e1beb93fe30e
s130604 <s130604@S130604.campus.tue.nl>
2015-06-05 13:13:18 +0200

    Finished timer

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -307,10 +307,10 @@ public class MainFrame {
      * @param args the command line arguments
      *
      */
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         MainFrame mainFrame = new MainFrame();
         mainFrame.readInput();
 //       
-    }
+    }*/
 
 }
